6739c9dd0455fdc641a4b6640d848e5a22bdac9d
Igor Entaltsev <igor.yentaltsev@gmail.com>
2013-06-07 10:28:21 -0400

    130607f 10:28 - added apricot.js

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ apricot
 =======
 
 A more advanced app than apple
+
+
+130607 10:20 when running it, the site wouldn't respond
+
---
+++ apricot.js
@@ -0,0 +1,42 @@
+
+
+
+var http = require("http"); // 1
+
+http.createServer(function (request, response) { // 2
+
+   request.on("end", function () { // 3
+
+      response.writeHead(200, { // 4
+         'Content-Type': 'text/plain' // 7
+      });
+
+      response.end('Apricot: Hello HTTP!'); // 5
+   });
+
+
+}).listen(8080); // 6
+
+console.log('Server listening. (http://localhost:8080)'); // 8
+
+/*
+ =================================================================
+ 1) Include http module.
+ 2) Create the server. 
+    A function is passed as parameter. This function is called on every request made.
+    $request holds all request parameters.
+    $response allows you to do anything with response sent to the client.
+ 3) Attach listener on end event.
+    This event is called when client sent all data and is waiting for response.
+ 4) Write headers to the response.
+    200 is HTTP status code (this one means success)
+ 5) Send data and end response.
+ 6) Listen on the 8080 port.
+ 7) Second parameter, holds header fields in object
+    Content-Type is 'text/plain' because we are sending plain text
+ 8) This text will show up in the terminal
+
+
+
+
+*/
