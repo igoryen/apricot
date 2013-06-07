093a3cc675287d47ec026f0fcc14d65afba005ff
Igor Entaltsev <igor.yentaltsev@gmail.com>
2013-06-07 10:33:20 -0400

    130607f 10:33 - compacted apricot.js

--- apricot.js
+++ apricot.js
@@ -2,21 +2,14 @@
 
 
 var http = require("http"); // 1
-
 http.createServer(function (request, response) { // 2
-
    request.on("end", function () { // 3
-
       response.writeHead(200, { // 4
          'Content-Type': 'text/plain' // 7
       });
-
       response.end('Apricot: Hello HTTP!'); // 5
    });
-
-
 }).listen(8080); // 6
-
 console.log('Server listening. (http://localhost:8080)'); // 8
 
 /*
@@ -35,8 +28,5 @@ console.log('Server listening. (http://localhost:8080)'); // 8
  7) Second parameter, holds header fields in object
     Content-Type is 'text/plain' because we are sending plain text
  8) This text will show up in the terminal
-
-
-
 
 */
